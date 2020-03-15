1d6ee3a35a45284c0b19fb3b4f5be831bcb97b37
KochiTongpoo <61816848+KochiTongpoo@users.noreply.github.com>
2020-03-15 20:42:31 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
-#coding:utf-8
+# -*- coding: utf-8 -*-
+
 import os
 from bottle import route, run
 
+
 @route("/")
 def hello_world():
-        return "hello world"
+    return ""
 
-run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
